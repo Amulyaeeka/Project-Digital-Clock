bb2d5c035afbead1bb1223766e9c5ebe29212963
AMULYA <24ec01017@iitbbs.ac.in>
2026-06-10 20:12:28 +0530

    Add files via upload

--- Digitalclock_clkdiv.v
+++ Digitalclock_clkdiv.v
@@ -1,9 +1,24 @@
-module Digitalclock(
-input clk,
-output q
+module Digitalclock_clkdiv (
+    input clk,          // 50 MHz clock
+    input rst,
+    output reg clk_1Hz
 );
-always @(posedgeclk,negedgeclk) begin
-    q = ~(qbar)
- for 
+
+reg [24:0] count;
+
+always @(posedge clk or posedge rst)
+begin
+    if (rst) begin
+        count   <= 25'd0;
+        clk_1Hz <= 1'b0;
+    end
+    else if (count == 25_000_000 - 1) begin
+        count   <= 25'd0;
+        clk_1Hz <= ~clk_1Hz;
+    end
+    else begin
+        count <= count + 1;
+    end
 end
+
 endmodule
